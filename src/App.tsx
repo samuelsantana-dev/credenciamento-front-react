a56fadfed2a8448bd91504c7b7e94ac945b530c7
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-04-16 16:46:36 -0300

    Nova forma de tabela

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { RegisterEmployess } from './pages/registerEmployees';
 import { CompanyRegisterForm } from './pages/registerCompany';
-import { ListOfAllCompanies } from './pages/listOfAllCompanies';
+// import { ListOfAllCompanies } from './pages/listOfAllCompanies';
 import { EmployeeListCompany } from './pages/listCompanyEmployees';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/homePage';
 import NavBar from './components/navBar';
+import DataTable from './pages/listAllEmployess';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       <Routes>
         <Route path="/register-employee" element={<RegisterEmployess />} />
         <Route path="/register-company" element={<CompanyRegisterForm />} />
-        <Route path="/companies" element={<ListOfAllCompanies />} />
+        {/* <Route path="/companies" element={<ListOfAllCompanies />} /> */}
+        <Route path="/companies" element={<DataTable />} />
         <Route path="/company/:companyId/employees" element={<EmployeeListCompany />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
