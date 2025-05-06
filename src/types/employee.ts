dc2ceba37af26555accbbf336d079fcbcb17d447
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-05-06 09:32:58 -0300

    Novo arquivos de funcionarios

---
+++ src/types/employee.ts
@@ -0,0 +1,9 @@
+export interface Employee {
+    id?: number;
+    name_employee: string;
+    email_employee: string;
+    telephone_employee: number;
+    position_employee: string;
+    department_employee: string;
+  }
+  
